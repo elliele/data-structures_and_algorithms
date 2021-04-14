0bb8f0acdc6ec258c4fd6fee6cf8e97fef239e26
Ellie Le <69995236+elliele@users.noreply.github.com>
2021-04-13 21:45:00 -0700

    Add files via upload

---
+++ ch_03/unique.py
@@ -0,0 +1,26 @@
+# Created by Ellie Le at 4/13/2021
+
+"""
+In the element
+uniqueness problem, we are given a single sequence S with n elements and asked
+whether all elements of that collection are distinct from each other.
+
+"""
+
+def unique1(S):
+    """Return True if there are no duplicate elements in sequence S."""
+    for j in range(len(S)):
+        for k in range(j+1, len(S)):
+            if S[j] == S[k]:
+                return False            # found duplicate pair
+    return True                         # if we reach this, elements were unique
+
+
+def unique2(S):
+    """Return True if there are no duplicate elements in sequence S."""
+    temp = sorted(S)                    # crete a sorted copy of S
+    for j in range(1, len(temp)):
+        if S[j-1] == S[j]:
+            return False                # found duplicate pair
+    return True                         # if we reach this, elements were unique
+
